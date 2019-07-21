3941689f2c30b25f709518271ed83a42d51a852a
MrRussLuther <rluther@broadinstitute.org>
2019-07-21 12:55:18 -0400

    packages from example

---
+++ myTopo.py
@@ -0,0 +1,26 @@
+from mininet.net import Mininet
+from mininet.node import Node, Switch, RemoteController
+from mininet.link import Link, Intf
+from mininet.log import setLogLevel, info
+from mininet.cli import CLI
+
+import mininet.ns3
+from mininet.ns3 import WIFISegment
+
+import ns.core
+import ns.network
+import ns.wifi
+import ns.csma
+import ns.wimax
+import ns.uan
+import ns.netanim
+
+from mininet.opennet import *
+
+def main():
+    print("Hello")
+
+
+if __name__ == '__main__':
+    setLogLevel('info')
+    main()
